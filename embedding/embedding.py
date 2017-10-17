0d77632a001765904343b57b43c17a913302d10c
Bryan He <bryanhe@stanford.edu>
2017-10-16 21:22:35 -0700

    saving output

--- embedding/embedding.py
+++ embedding/embedding.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 
 import torch
+import numpy as np
 import time
 import os
+import struct
 
 class Embedding(object):
     def __init__(self, dim=300):
@@ -13,15 +15,17 @@ class Embedding(object):
 
         self.cooccurrence = cooccurrence
         self.vocab = vocab
+        self.prev = None
 
         if embedding is None:
-            self.embedding = torch.zeros([self.n, self.dim]).type(torch.DoubleTensor)
-            for i in range(min(self.n, self.dim)):
-                self.embedding[i, i] = 1
+            self.embedding = torch.randn([self.n, self.dim]).type(torch.DoubleTensor)
+            self.normalize()
         else:
             self.embedding = embedding
 
     def load_from_file(self):
+
+        begin = time.time()
 
         def parse_line(l):
             l = l.split()
@@ -31,17 +35,44 @@ class Embedding(object):
         with open("vocab.txt") as f:
             lines = [parse_line(l) for l in f]
             self.words = [l[0] for l in lines]
-            self.vocab = torch.DoubleTensor([l[1] for l in lines])
+            vocab = torch.DoubleTensor([l[1] for l in lines])
+        n = vocab.size()[0]
 
-        print(os.stat("cooccurrence.shuf.bin"))
-        v = torch.randn([nnz])
-        v = v.type(torch.DoubleTensor)
-        ind = torch.rand(1, nnz) * torch.Tensor([n, n]).repeat(nnz, 1).transpose(0, 1)
-        ind = ind.type(torch.LongTensor)
-        with open("cooccurrence.shuf.bin") as f:
+        filesize = os.stat("cooccurrence.shuf.bin").st_size
+        assert(filesize % 16 == 0)
+        nnz = filesize / 16
+        # v = np.zeros(nnz, np.float64)
+        # v = torch.zeros([nnz])
+        # v = v.type(torch.DoubleTensor)
+        # ind = torch.zeros(2, nnz)
+        # ind = ind.type(torch.LongTensor)
+        v = np.empty(nnz, np.float64)
+        ind = np.empty((2, nnz), np.int64) # TODO: binary format is int32, but torch uses Long
+        with open("cooccurrence.shuf.bin", "rb") as f:
+            content = f.read()
+            # for i in range(min(nnz, 10000000)):
+            # for i in range(min(nnz, 100000)):
+            i = 0
+            block = 100
+            while i < nnz:
+                block = min(block, nnz - i)
+                line = struct.unpack("iid" * block, content[(16 * i):(16 * (i + block))])
+                ind[0, i:(i + block)] = line[0::3]
+                ind[1, i:(i + block)] = line[1::3]
+                v[i:(i + block)] = line[2::3]
+                i += block
+            ind = ind - 1
+        v = torch.DoubleTensor(v)
+        ind = torch.LongTensor(ind)
+        cooccurrence = torch.sparse.DoubleTensor(ind, v, torch.Size([n, n])).coalesce()
 
+        self.load(cooccurrence, vocab)
+
+        end = time.time()
+        print("Loading data took", end - begin)
 
     def normalize(self):
+        # TODO: is it necessary to reorder columns by magnitude
         begin = time.time()
         self.embedding, r = torch.qr(self.embedding)
         if self.prev is not None:
@@ -49,7 +80,7 @@ class Embedding(object):
         end = time.time()
         print("Normalizing took", end - begin)
 
-    def power_iteration(self, iterations=1, beta=0.1, norm_freq=1):
+    def power_iteration(self, iterations=1, beta=0.0, norm_freq=1):
         begin = time.time()
         self.cooccurrence.cuda()
         self.embedding.cuda()
@@ -78,10 +109,12 @@ class Embedding(object):
             self.normalize()
 
         # Scale correctly
+        begin = time.time()
         self.embedding = torch.mm(self.cooccurrence, self.embedding)
+        end = time.time()
+        print("Final scaling:", end - begin)
 
         begin = time.time()
-        self.cooccurrence.cpu()
         self.embedding.cpu()
         end = time.time()
         print("CPU Loading:", end - begin)
@@ -89,11 +122,13 @@ class Embedding(object):
     def vr(self):
         pass
 
-    def dump_to_file(self):
-        pass
-
-    def get_embeddings(self):
-        pass
+    def save_to_file(self):
+        begin = time.time()
+        with open("vectors.txt", "w") as f:
+            for i in range(self.n):
+                f.write(self.words[i] + " " + " ".join([str(self.embedding[i, j]) for j in range(self.dim)]) + "\n")
+        end = time.time()
+        print("Saving embeddings:", end - begin)
 
 
 def synthetic(n, nnz):
@@ -111,7 +146,7 @@ def synthetic(n, nnz):
     cooccurrence = torch.sparse.DoubleTensor(ind, v, torch.Size([n, n])).coalesce()
     vocab = None
     end = time.time()
-    print("Generating syntetic data:", end - begin)
+    print("Generating synthetic data:", end - begin)
 
     return cooccurrence, vocab
 
@@ -120,6 +155,6 @@ def main(argv=None):
 
     embedding = Embedding()
     embedding.load_from_file()
-    embedding.load(*synthetic(500000, 10000000))
+    # embedding.load(*synthetic(500000, 10000000))
     embedding.power_iteration()
-    embedding.dump_to_file()
+    embedding.save_to_file()
