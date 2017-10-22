2726f6354c109ed25e71e2a63f325d3269aea19e
Bryan He <bryanhe@stanford.edu>
2017-10-21 21:47:07 -0700

    travis setup

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='embedding',
-    version='0.0',
-    description='compute word embeddings',
+    name="embedding",
+    version="0.0",
+    description="compute word embeddings",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "torch"
+    ],
     entry_points={
-        'console_scripts': [
-            'embedding = embedding.embedding:main',
+        "console_scripts": [
+            "embedding = embedding.embedding:main",
         ],
     },
 )
