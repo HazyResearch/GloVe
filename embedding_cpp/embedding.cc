a6892d097548ab4244024484a0fe158086886f11
Bryan He <bryanhe@stanford.edu>
2017-11-03 17:27:00 -0700

    cmd line (https://github.com/jarro2783/cxxopts)

--- embedding_cpp/embedding.cc
+++ embedding_cpp/embedding.cc
@@ -1,3 +1,4 @@
+#include "cxxopts.h"
 #include "cusparse.h"
 #include "sparse.h"
 #include <cuda_runtime.h>
@@ -7,29 +8,52 @@
 #include "utils.h"
 #include <omp.h>
 
-int main(void) {
+int main(int argc, char *argv[], char *envp[]) {
   size_t n_dimensions = 50;
   size_t n_iterations = 10;
-  bool gpu = false;
+  size_t n_threads = 48;
+  bool gpu = true;
 
-  mkl_set_num_threads(48);  // set num threads
-  omp_set_num_threads(48);  // set num threads
+  std::string cooccurrence_file = "cooccurrence.shuf.bin";
+  std::string vocab_file = "vocab.txt";
+  std::string output_file = "vectors.txt";
+
+  try {
+    cxxopts::Options parser(argv[0], "Compute embeddings");
+
+    parser.add_options()
+      ("h,help", "print help")
+      ("d,dim", "number of dimensions in embedding", cxxopts::value<size_t>(n_dimensions)->default_value("50"))
+      ("n,iter", "number of iterations", cxxopts::value<size_t>(n_iterations)->default_value("10"))
+      ("t,threads", "number of CPU threads", cxxopts::value<size_t>(n_threads)->default_value("48"))
+      // ("g,gpu", "toogle GPU use", cxxopts::value<bool>(gpu)->default_value(true))
+      ("c,cooccurrence", "name of cooccurrence file", cxxopts::value<std::string>(cooccurrence_file)->default_value("cooccurrence.shuf.bin"))
+      ("v,vocab", "name of vocab file", cxxopts::value<std::string>(vocab_file)->default_value("vocab.txt"))
+      ("o,vectors", "name of vectors output file", cxxopts::value<std::string>(output_file)->default_value("vectors.txt"))
+    ;
+    parser.parse(argc, argv);
+
+    if (parser.count("help") != 0) {
+      std:: cout << parser.help() << std::endl;
+      exit(0);
+    }
+  }
+  catch (const cxxopts::OptionException& e) {
+    std::cout << "error parsing options: " << e.what() << std::endl;
+    exit(1);
+  }
+
+  std::cout << "dimension: " << n_dimensions << std::endl;
+  std::cout << "iterations: " << n_iterations << std::endl;
+  std::cout << "threads: " << n_threads << std::endl;
+
+  mkl_set_num_threads(n_threads);  // set num threads
+  omp_set_num_threads(n_threads);  // set num threads
   const auto loading = timer::start_clock();
-  const std::string coocurrence_file =
-      "/dfs/scratch0/caberger/datasets/coocurrence/wikipedia/"
-      "cooccurrence.shuf.bin";
-  const std::string vocab_file =
-      "/dfs/scratch0/caberger/datasets/coocurrence/wikipedia/"
-      "vocab.txt";
-  const std::string output_file = "out.txt";
-  // const std::string coocurrence_file =
-  //    "/dfs/scratch0/caberger/datasets/coocurrence/pubmed/"
-  //    "cooccurrence.shuf.bin";
 
-  COO<double> coo_cooccurrence = COO<double>::from_file(coocurrence_file);
+  COO<double> coo_cooccurrence = COO<double>::from_file(cooccurrence_file);
   std::vector<std::string> vocab = utils::load_vocab(vocab_file);
-  timer::stop_clock("LOADING",loading);
-
+  timer::stop_clock("LOADING", loading);
   
   std::cout << "nnz: " << coo_cooccurrence.nnz << std::endl;
   std::cout << "n: " << coo_cooccurrence.n << std::endl;
@@ -37,7 +61,7 @@ int main(void) {
   const auto csr_build = timer::start_clock();
   CSR<double> csr_cooccurrence = CSR<double>::from_coo(coo_cooccurrence);
   /*debug*/ //csr_cooccurrence.print();
-  timer::stop_clock("CSR BUILD",csr_build);
+  timer::stop_clock("CSR BUILD", csr_build);
 
   const auto preprocessing = timer::start_clock();
   if(!gpu)
