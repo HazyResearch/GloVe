4c311fdb45e8689ddcebd0ed1a24bba177da4168
Bryan He <bryanhe@stanford.edu>
2017-11-05 16:33:47 -0800

    split out code

--- embedding/embedding.py
+++ embedding/embedding.py
@@ -17,90 +17,12 @@ import embedding.solver as solver
 import embedding.util as util
 import embedding.evaluate as evaluate
 import embedding.tensor_type as tensor_type
-from embedding.__version__ import __version__
+import embedding.parser as parser
 
 
 def main(argv=None):
 
-    parser = argparse.ArgumentParser(description="Tools for embeddings.")
-
-    # Add version to parser
-    parser.add_argument("-v", "--version",
-                        action='version',
-                        version="%(prog)s " + __version__,
-                        help="Print version number.")
-
-    subparser = parser.add_subparsers(dest="task")
-
-    # Cooccurrence parser
-    cooccurrence_parser = subparser.add_parser("cooccurrence", help="Preprocessing (compute vocab and cooccurrence from text).")
-
-    cooccurrence_parser.add_argument("text", type=str, nargs="?", default="text", help="filename of text file")
-
-    # Compute parser
-    compute_parser = subparser.add_parser("compute", help="Compute embedding from scratch via cooccurrence matrix.")
-
-    compute_parser.add_argument("-d", "--dim", type=int, default=50,
-                                help="dimension of embedding")
-
-    compute_parser.add_argument("--vocab", type=str, default="vocab.txt",
-                                help="filename of vocabulary file")
-    compute_parser.add_argument("-c", "--cooccurrence", type=str, default="cooccurrence.shuf.bin",
-                                help="filename of cooccurrence binary")
-    compute_parser.add_argument("--initial", type=str, default=None,
-                                help="filename of initial embedding vectors")
-    compute_parser.add_argument("--initialbias", type=str, default=None,
-                                help="filename of initial bias")
-    compute_parser.add_argument("-o", "--vectors", type=str, default="vectors.txt",
-                                help="filename for embedding vectors output")
-    compute_parser.add_argument("--bias", type=str, default="bias.txt",
-                                help="filename for bias output")
-
-    compute_parser.add_argument("-p", "--preprocessing", type=str.lower, default="ppmi",
-                                choices=["none", "log1p", "ppmi"],
-                                help="Preprocessing of cooccurrence matrix before eigenvector computation")
-
-    compute_parser.add_argument("-s", "--solver", type=str.lower, default="pi",
-                                choices=["pi", "alecton", "vr", "sgd", "glove", "sparsesvd", "gemsim"],
-                                help="Solver used to find top eigenvectors")
-    compute_parser.add_argument("-i", "--iterations", type=int, default=50,
-                                help="Iterations used by solver")
-    compute_parser.add_argument("-e", "--eta", "--step", type=float, default=1e-3,
-                                help="Learning rate used by solver")
-    compute_parser.add_argument("-m", "--momentum", "--beta", type=float, default=0.,
-                                help="Momentum used by solver")
-    compute_parser.add_argument("-f", "--normfreq", type=int, default=1,
-                                help="Normalization frequency used by solver")
-    compute_parser.add_argument("-b", "--batch", type=int, default=100000,
-                                help="Batch size used by solver")
-    compute_parser.add_argument("-j", "--innerloop", type=int, default=10,
-                                help="Inner loop iterations used by solver")
-
-    compute_parser.add_argument("--scale", type=float, default=0.5,
-                                help="Scale on eigenvector is $\lambda_i ^ s$")
-    compute_parser.add_argument("-n", "--normalize", type=util.str2bool, default=False,
-                                help="Toggle to normalize embeddings")
-
-    compute_parser.add_argument("-g", "--gpu", type=util.str2bool, default=True,
-                                help="Toggle to use GPU for computations")
-    compute_parser.add_argument("--matgpu", type=util.str2bool, default=None,
-                                help="Toggle to store cooccurrence matrix on GPU")
-    compute_parser.add_argument("--embedgpu", type=util.str2bool, default=None,
-                                help="Toggle to store embeddings on GPU")
-
-    compute_parser.add_argument("--precision", type=str.lower, default="float",
-                                choices=["float", "double"],
-                                help="Precision of values")
-
-    # Evaluate parser
-    evaluate_parser = subparser.add_parser("evaluate", help="Evaluate performance of an embedding on standard tasks.")
-
-    evaluate_parser.add_argument('--vocab', type=str, default='vocab.txt',
-                                 help="filename of vocabulary file")
-    evaluate_parser.add_argument('--vectors', type=str, default='vectors.txt',
-                                 help="filename of embedding vectors file")
-
-    args = parser.parse_args(argv)
+    args = parser.get_parser().parse_args(argv)
 
     # Prepare logger
     logging.getLogger(__name__).addHandler(logging.NullHandler())
