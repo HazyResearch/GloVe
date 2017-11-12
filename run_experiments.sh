274e65a1448ac18f02a7acbbfd6b3b5d5ed16374
Bryan He <bryanhe@stanford.edu>
2017-11-12 03:14:57 -0800

    scripts with parameter choices

---
+++ run_experiments.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+embedding compute -i 1000 --checkpoint 1 --scale 0 -o output/pi
+embedding compute -i 100000 --checkpoint 100 --scale 0 -o output/pi
+
+# 50th eigenvalue: 393.15
+# Using 390 ^ 2 / 4
+embedding compute -i 1000 --checkpoint 1 --scale 0 --beta 38025 -o output/pim
+
+# Distinct Words: 71290
+# Number of non-zeros: 60666466
+
+embedding compute --solver alecton --scheme element --batch 606665 --eta 0.0001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.606665.0001
+embedding compute --solver alecton --scheme row     --batch 713    --eta 0.0001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.713.0001
+embedding compute --solver alecton --scheme column  --batch 713    --eta 0.0001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.713.0001
+
+embedding compute --solver alecton --scheme element --batch 606665 --eta 0.00001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.606665.00001
+embedding compute --solver alecton --scheme row     --batch 713    --eta 0.00001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.713.00001
+embedding compute --solver alecton --scheme column  --batch 713    --eta 0.00001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.713.00001
+
+embedding compute --solver alecton --scheme element --batch 606665 --eta 0.000001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.606665.000001
+embedding compute --solver alecton --scheme row     --batch 713    --eta 0.000001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.713.000001
+embedding compute --solver alecton --scheme column  --batch 713    --eta 0.000001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.713.000001
+
+
+embedding compute --solver alecton --scheme element --batch 6066647 --eta 0.001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.6066647.001
+embedding compute --solver alecton --scheme row     --batch 7129    --eta 0.001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.7129.001
+embedding compute --solver alecton --scheme column  --batch 7129    --eta 0.001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.7129.001
+
+embedding compute --solver alecton --scheme element --batch 6066647 --eta 0.0001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.6066647.0001
+embedding compute --solver alecton --scheme row     --batch 7129    --eta 0.0001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.7129.0001
+embedding compute --solver alecton --scheme column  --batch 7129    --eta 0.0001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.7129.0001
+
+embedding compute --solver alecton --scheme element --batch 6066647 --eta 0.00001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.6066647.00001
+embedding compute --solver alecton --scheme row     --batch 7129    --eta 0.00001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.7129.00001
+embedding compute --solver alecton --scheme column  --batch 7129    --eta 0.00001 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.7129.00001
+
+
+embedding compute --solver alecton --scheme element --batch 30333233 --eta 0.005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.30333233.005
+embedding compute --solver alecton --scheme row     --batch 35645    --eta 0.005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.35645.005
+embedding compute --solver alecton --scheme column  --batch 35645    --eta 0.005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.35645.005
+
+embedding compute --solver alecton --scheme element --batch 30333233 --eta 0.0005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.30333233.0005
+embedding compute --solver alecton --scheme row     --batch 35645    --eta 0.0005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.35645.0005
+embedding compute --solver alecton --scheme column  --batch 35645    --eta 0.0005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.35645.0005
+
+embedding compute --solver alecton --scheme element --batch 30333233 --eta 0.00005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.ele.30333233.00005
+embedding compute --solver alecton --scheme row     --batch 35645    --eta 0.00005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.row.35645.00005
+embedding compute --solver alecton --scheme column  --batch 35645    --eta 0.00005 -i 25000 --checkpoint 100 --normfreq 100 --scale 0 -o output/alecton.col.35645.00005
